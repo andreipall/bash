c564e45077ee7f32d7e7347c68750d94844edc7c
Andrei Pall <andreipall3@gmail.com>
2026-04-18 10:20:56 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -8,8 +8,9 @@ mkfs.fat -F32 /dev/sdb1
 mkfs.ext4 /dev/sdb2
 pacman -Syy
 mount /dev/sdb2 /mnt
+pacstrap -K /mnt base
 touch /mnt/etc/vconsole.conf
-pacstrap -K /mnt base linux linux-firmware-intel linux-firmware-realtek sudo nano
+pacstrap -K /mnt linux linux-firmware-intel linux-firmware-realtek sudo nano
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
