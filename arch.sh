31fc4f9540751605092cf2d7b00e263899b73990
Andrei Pall <andreipall3@gmail.com>
2026-04-24 20:05:31 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 # This Zsh script installs the Xfce desktop on Arch Linux, by Andrei Pall
+PASSWORD=""
+set -euo pipefail
 if [[ ! -b /dev/sdb1 || ! -b /dev/sdb2 ]]; then
     echo "Required partitions not found!"
     exit 1
@@ -21,8 +23,8 @@ locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
 echo andrei-pc > /etc/hostname
-passwd
-pacman -S grub efibootmgr intel-ucode
+echo "root:$PASSWORD" | chpasswd
+pacman -S --noconfirm grub efibootmgr intel-ucode
 mkdir /boot/efi
 mount /dev/sdb1 /boot/efi
 grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
@@ -36,17 +38,17 @@ DHCP=yes
 NET
 systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
-pacman -S xorg-server xf86-video-intel
+pacman -S --noconfirm xorg-server xf86-video-intel
 useradd -m -c "Andrei Pall" -G wheel andrei
-passwd andrei
+echo "andrei:$PASSWORD" | chpasswd
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sed -i 's/^GRUB_PRELOAD_MODULES="part_gpt part_msdos"/GRUB_PRELOAD_MODULES="part_gpt"/' /etc/default/grub
 sed -i 's/^GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=hidden/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S exo garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop xfwm4
-pacman -S mousepad ristretto thunar-archive-plugin xfce4-pulseaudio-plugin xfce4-screenshoter xfce4-notifyd
-pacman -S lightdm-gtk-greeter network-manager-applet noto-fonts parole gst-libav gst-plugins-bad gst-plugins-ugly firefox gvfs gvfs-mtp xarchiver unrar unzip pulseaudio pavucontrol transmission-gtk
+pacman -S --noconfirm exo garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop xfwm4
+pacman -S --noconfirm mousepad ristretto thunar-archive-plugin xfce4-pulseaudio-plugin xfce4-screenshoter xfce4-notifyd
+pacman -S --noconfirm lightdm-gtk-greeter network-manager-applet noto-fonts parole gst-libav gst-plugins-bad gst-plugins-ugly firefox gvfs gvfs-mtp xarchiver unrar unzip pulseaudio pavucontrol transmission-gtk
 systemctl enable lightdm.service
 systemctl enable NetworkManager
 sed -i 's/^#xserver-command=X/xserver-command=X -s 0 -dpms/' /etc/lightdm/lightdm.conf
@@ -54,7 +56,7 @@ sed -i 's/^#autologin-user=/autologin-user=andrei/' /etc/lightdm/lightdm.conf
 sed -i 's/^#autologin-session=/autologin-session=xfce/' /etc/lightdm/lightdm.conf
 groupadd -r autologin
 gpasswd -a andrei autologin
-pacman -S ufw
+pacman -S --noconfirm ufw
 systemctl enable ufw.service
 systemctl start ufw.service
 systemctl status ufw
