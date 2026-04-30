59ce4920edb4704e8a1bd7381fac581b7b2a65ee
Andrei Pall <andreipall3@gmail.com>
2026-04-30 19:20:27 +0300

    Add files via upload

---
+++ global.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script with sudo"
+  exit 1
+fi
+
+# Detect the original user (who invoked sudo)
+ORIGINAL_USER="${SUDO_USER:-$(whoami)}"
+
+echo "Running as root: $(whoami)"
+
+# Run a script as root
+echo "Running root scripts..."
+./mount.sh
+./themes.sh
+
+# Running scripts as the original user
+echo "Running user script as $ORIGINAL_USER..."
+sudo -u "$ORIGINAL_USER" ./hide_xfce_menu_icons.sh
+sudo -u "$ORIGINAL_USER" ./xfce_customization.sh
