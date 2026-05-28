789bd110621c56f9e97e01c19ceffd878528f20b
Andrei Pall <andreipall3@gmail.com>
2026-05-28 14:22:18 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -36,9 +36,9 @@ sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 sed -i 's/^GRUB_PRELOAD_MODULES="part_gpt part_msdos"/GRUB_PRELOAD_MODULES="part_gpt"/' /etc/default/grub
 sed -i 's/^GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=hidden/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S --noconfirm exo garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop xfwm4 sassc
+pacman -S --noconfirm pipewire-pulse exo garcon thunar thunar-volman tumbler xfce4-appfinder xfce4-panel xfce4-session xfce4-settings xfce4-terminal xfconf xfdesktop xfwm4 sassc
 pacman -S --noconfirm mousepad ristretto thunar-archive-plugin xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-notifyd
-pacman -S --noconfirm lightdm-gtk-greeter network-manager-applet noto-fonts parole gst-libav gst-plugins-bad gst-plugins-ugly firefox gvfs gvfs-mtp xarchiver unrar unzip pulseaudio pavucontrol transmission-gtk python-requests python-chardet bleachbit
+pacman -S --noconfirm lightdm-gtk-greeter network-manager-applet noto-fonts parole gst-libav gst-plugins-bad gst-plugins-ugly firefox gvfs gvfs-mtp xarchiver unrar unzip pavucontrol transmission-gtk python-requests python-chardet bleachbit
 systemctl enable lightdm.service
 systemctl enable NetworkManager
 systemctl set-default graphical.target
