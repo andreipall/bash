50f546e015146874d2a9df319854e05678e8b136
Andrei Pall <andreipall3@gmail.com>
2026-08-29 20:41:48 +0300

    Add files via upload

--- global.sh
+++ global.sh
@@ -7,6 +7,7 @@ fi
 
 # Detect the original user (who invoked sudo)
 ORIGINAL_USER="${SUDO_USER:-$(whoami)}"
+USER_UID=$(id -u "$ORIGINAL_USER")
 
 echo "Running as root: $(whoami)"
 
@@ -17,4 +18,5 @@ echo "Running root scripts..."
 # Running scripts as the original user
 echo "Running user script as $ORIGINAL_USER..."
 sudo -u "$ORIGINAL_USER" ./hide_xfce_menu_icons.sh
-sudo -E -u "$ORIGINAL_USER" ./xfce_customization.sh
+# sudo -E -u "$ORIGINAL_USER" ./xfce_customization.sh
+sudo -u "$ORIGINAL_USER" DISPLAY="$DISPLAY" XDG_RUNTIME_DIR="/run/user/$USER_UID" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USER_UID/bus" XAUTHORITY="/home/$ORIGINAL_USER/.Xauthority" ./xfce_customization.sh
